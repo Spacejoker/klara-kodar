1eb395f94a4e287a5cf19362dd768653c26fdf62
js <stahl.jens@gmail.com>
2019-09-29 12:25:19 -0700

    Add humla.py

---
+++ humla.py
@@ -0,0 +1,29 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
+print "humla"
+
+# for i in range (0,5):
+#     print "KLARA is 5 years old"
+# 
+# for i in range (0,1):
+#     print "AXEL is 1 years old"
+# 
+# for i in range (0,7):
+#     print "IRENE is 7 years old"
+# 
+# for i in range (0,35):
+#     print "PAPPA is 35 years old"
+#     
+# for i in range (0,9):
+#     print "MAMMA is 31 years old"
+# 
+# for i in range (0,29):
+#     print bcolors.OKGREEN + "KARINA is 5 years old" + bcolors.ENDC
